eaad13b174b50a7a135c54b8e3887a3338a9fcd7
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-04-16 10:12:02 +0300

    feature: update app files

--- bookreview_client/src/App.js
+++ bookreview_client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Toaster} from "react-hot-toast";
+import AllBookPage from "./page/AllBookPage";
+import BookPage from "./page/BookPage";
+import Navigation from "./component/sidebar/Navigation";
+import AllBookAdminPage from "./page/admin/AllBookAdminPage";
+import AllAuthorAdminPage from "./page/admin/AllAuthorAdminPage";
+import AuthorPage from "./page/AuthorPage";
+import StartPage from "./page/StartPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Toaster
+              position="top-center"
+              reverseOrder={false}
+          />
+          <Navigation/>
+          <Routes>
+              <Route path="/" element={<StartPage/>}/>
+              <Route path="/book" element={<AllBookPage/>}/>
+              <Route path="/book/:id" element={<BookPage/>}/>
+              <Route path="/author/:id" element={<AuthorPage/>}/>
+              <Route path="/admin/book" element={<AllBookAdminPage/>}/>
+              <Route path="/admin/author" element={<AllAuthorAdminPage/>}/>
+          </Routes>
+      </Router>
   );
 }
 
